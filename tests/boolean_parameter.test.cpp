0113086ae579098b917270646d6fc54fa346c833
Eldar Zakirov <unixod@gmail.com>
2023-01-09 23:29:19 +0300

    T6

--- tests/boolean_parameter.test.cpp
+++ tests/boolean_parameter.test.cpp
@@ -9,10 +9,10 @@ using ez::cli::api::Parameter;
 using namespace std::string_view_literals;
 using namespace std::string_literals;
 
-TEST_CASE("Mandatory boolean parameter (i.e. has no default value) may have only short name")
+TEST_CASE("Boolean parameter may have only short name")
 {
     using P = ez::cli::Boolean_parameter<"short-name", "",
-        "A descritption of the param.",
+        "The parameter descritption.",
         [](std::true_type) {
             return 123;
         },
@@ -22,7 +22,7 @@ TEST_CASE("Mandatory boolean parameter (i.e. has no default value) may have only
     >;
 
     STATIC_REQUIRE(P::short_name == "short-name");
-    STATIC_REQUIRE(P::description == "A descritption of the param.");
+    STATIC_REQUIRE(P::description == "The parameter descritption.");
     STATIC_REQUIRE(P::true_value() == 123);
     STATIC_REQUIRE(P::false_value() == 321);
 
@@ -43,22 +43,26 @@ TEST_CASE("Mandatory boolean parameter (i.e. has no default value) may have only
     STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
 }
 
-TEST_CASE("Mandatory regular parameter (i.e. has no default value) may have only long name")
+TEST_CASE("Boolean parameter may have only long name")
 {
-    using P = ez::cli::Regular_parameter<"", "long-name",
-        "A descritption of the param.",
-        [](auto arg) {
-            return arg;
+    using P = ez::cli::Boolean_parameter<"", "long-name",
+        "The parameter descritption.",
+        [](std::true_type) {
+            return 123;
+        },
+        [](std::false_type) {
+            return 321;
         }
     >;
 
     STATIC_REQUIRE(P::long_name == "long-name");
-    STATIC_REQUIRE(P::description == "A descritption of the param.");
-    STATIC_REQUIRE(P::parse_value("123") == "123");
+    STATIC_REQUIRE(P::description == "The parameter descritption.");
+    STATIC_REQUIRE(P::true_value() == 123);
+    STATIC_REQUIRE(P::false_value() == 321);
 
     STATIC_REQUIRE_FALSE(Positional_parameter<P>);
-    STATIC_REQUIRE(Regular_parameter<P>);
-    STATIC_REQUIRE_FALSE(Boolean_parameter<P>);
+    STATIC_REQUIRE_FALSE(Regular_parameter<P>);
+    STATIC_REQUIRE(Boolean_parameter<P>);
     STATIC_REQUIRE(Parameter<P>);
 
     namespace details_ = ez::cli::api::details_;
@@ -66,30 +70,34 @@ TEST_CASE("Mandatory regular parameter (i.e. has no default value) may have only
     STATIC_REQUIRE(details_::Has_long_name<P>);
     STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
     STATIC_REQUIRE(details_::Has_description<P>);
-    STATIC_REQUIRE(details_::Has_value_parser<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_value_parser<P>);
     STATIC_REQUIRE_FALSE(details_::Has_default_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_true_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_false_value<P>);
+    STATIC_REQUIRE(details_::Has_true_value<P>);
+    STATIC_REQUIRE(details_::Has_false_value<P>);
     STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
 }
 
-TEST_CASE("Mandatory regular parameter (i.e. has no default value) may have both long&short names")
+TEST_CASE("Boolean parameter may have both short and long names")
 {
-    using P = ez::cli::Regular_parameter<"short-name", "long-name",
-        "A descritption of the param.",
-        [](auto arg) {
-            return arg;
+    using P = ez::cli::Boolean_parameter<"short-name", "long-name",
+        "The parameter descritption.",
+        [](std::true_type) {
+            return 123;
+        },
+        [](std::false_type) {
+            return 321;
         }
     >;
 
     STATIC_REQUIRE(P::short_name == "short-name");
     STATIC_REQUIRE(P::long_name == "long-name");
-    STATIC_REQUIRE(P::description == "A descritption of the param.");
-    STATIC_REQUIRE(P::parse_value("123") == "123");
+    STATIC_REQUIRE(P::description == "The parameter descritption.");
+    STATIC_REQUIRE(P::true_value() == 123);
+    STATIC_REQUIRE(P::false_value() == 321);
 
     STATIC_REQUIRE_FALSE(Positional_parameter<P>);
-    STATIC_REQUIRE(Regular_parameter<P>);
-    STATIC_REQUIRE_FALSE(Boolean_parameter<P>);
+    STATIC_REQUIRE_FALSE(Regular_parameter<P>);
+    STATIC_REQUIRE(Boolean_parameter<P>);
     STATIC_REQUIRE(Parameter<P>);
 
     namespace details_ = ez::cli::api::details_;
@@ -97,187 +105,65 @@ TEST_CASE("Mandatory regular parameter (i.e. has no default value) may have both
     STATIC_REQUIRE(details_::Has_long_name<P>);
     STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
     STATIC_REQUIRE(details_::Has_description<P>);
-    STATIC_REQUIRE(details_::Has_value_parser<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_value_parser<P>);
     STATIC_REQUIRE_FALSE(details_::Has_default_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_true_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_false_value<P>);
+    STATIC_REQUIRE(details_::Has_true_value<P>);
+    STATIC_REQUIRE(details_::Has_false_value<P>);
     STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
 }
 
-TEST_CASE("Optional regular parameter (i.e. has default value) may have only short name")
-{
-    using P = ez::cli::Regular_parameter<"short-name", "",
-        "A descritption of the param.",
-        [](auto arg) {
-            return arg;
-        },
-        [] {
-            return "default value"sv;
-        }
-    >;
-
-    STATIC_REQUIRE(P::short_name == "short-name");
-    STATIC_REQUIRE(P::description == "A descritption of the param.");
-    STATIC_REQUIRE(P::parse_value("123") == "123");
-
-    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
-    STATIC_REQUIRE(Regular_parameter<P>);
-    STATIC_REQUIRE_FALSE(Boolean_parameter<P>);
-    STATIC_REQUIRE(Parameter<P>);
-
-    namespace details_ = ez::cli::api::details_;
-    STATIC_REQUIRE(details_::Has_short_name<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_long_name<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
-    STATIC_REQUIRE(details_::Has_description<P>);
-    STATIC_REQUIRE(details_::Has_value_parser<P>);
-    STATIC_REQUIRE(details_::Has_default_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_true_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_false_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
-}
-
-TEST_CASE("Optional regular parameter (i.e. has default value) may have only long name")
-{
-    using P = ez::cli::Regular_parameter<"", "long-name",
-        "A descritption of the param.",
-        [](auto arg) {
-            return arg;
-        },
-        [] {
-            return "default value"sv;
-        }
-    >;
-
-    STATIC_REQUIRE(P::long_name == "long-name");
-    STATIC_REQUIRE(P::description == "A descritption of the param.");
-    STATIC_REQUIRE(P::parse_value("123") == "123");
-
-    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
-    STATIC_REQUIRE(Regular_parameter<P>);
-    STATIC_REQUIRE_FALSE(Boolean_parameter<P>);
-    STATIC_REQUIRE(Parameter<P>);
-
-    namespace details_ = ez::cli::api::details_;
-    STATIC_REQUIRE_FALSE(details_::Has_short_name<P>);
-    STATIC_REQUIRE(details_::Has_long_name<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
-    STATIC_REQUIRE(details_::Has_description<P>);
-    STATIC_REQUIRE(details_::Has_value_parser<P>);
-    STATIC_REQUIRE(details_::Has_default_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_true_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_false_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
-}
-
-TEST_CASE("Optional regular parameter (i.e. has default value) may have both long&short names")
-{
-    using P = ez::cli::Regular_parameter<"short-name", "long-name",
-        "A descritption of the param.",
-        [](auto arg) {
-            return arg;
-        },
-        [] {
-            return "default value"sv;
-        }
-    >;
-
-    STATIC_REQUIRE(P::short_name == "short-name");
-    STATIC_REQUIRE(P::long_name == "long-name");
-    STATIC_REQUIRE(P::description == "A descritption of the param.");
-    STATIC_REQUIRE(P::parse_value("123") == "123");
-
-    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
-    STATIC_REQUIRE(Regular_parameter<P>);
-    STATIC_REQUIRE_FALSE(Boolean_parameter<P>);
-    STATIC_REQUIRE(Parameter<P>);
-
-    namespace details_ = ez::cli::api::details_;
-    STATIC_REQUIRE(details_::Has_short_name<P>);
-    STATIC_REQUIRE(details_::Has_long_name<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
-    STATIC_REQUIRE(details_::Has_description<P>);
-    STATIC_REQUIRE(details_::Has_value_parser<P>);
-    STATIC_REQUIRE(details_::Has_default_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_true_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_false_value<P>);
-    STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
-}
-
+namespace {
 
 using Incorrect_parameter_types = std::tuple<
-    // Both short and long parameter names are missing (empty strings).
-    ez::cli::Regular_parameter<"", "",
-        "A descritption of the param.",
-        [](auto arg) { return arg; }
+     // Both short and long parameter names are missing (empty strings).
+    ez::cli::Boolean_parameter<"", "",
+        "The parameter descritption.",
+        [](std::true_type) { return 123; },
+        [](std::false_type) { return 321; }
     >,
 
     // A parameter description is missing (empty string).
-    ez::cli::Regular_parameter<"short-name", "",
+    ez::cli::Boolean_parameter<"short-name", "",
         "",
-        [](auto arg) {
-            return arg;
-        }
+        [](std::true_type) { return 123; },
+        [](std::false_type) { return 321; }
     >,
 
     // A parameter description is missing (empty string).
-    ez::cli::Regular_parameter<"", "long-name",
+    ez::cli::Boolean_parameter<"", "long-name",
         "",
-        [](auto arg) {
-            return arg;
-        }
-    >,
-
-    // An argument value parsing funciton is missing.
-    ez::cli::Regular_parameter<"short-name", "",
-        "A descritption of the param."
-    >,
-
-    //==-----------------------------------------------==//
-    //  Ditto as above but with default value specified  //
-    //==-----------------------------------------------==//
-
-    // Both short and long parameter names are missing (empty strings).
-    ez::cli::Regular_parameter<"", "",
-        "A descritption of the param.",
-        [](auto arg) {
-            return arg;
-        },
-        [] {
-            return "default value"sv;
-        }
+        [](std::true_type) { return 123; },
+        [](std::false_type) { return 321; }
     >,
 
     // A parameter description is missing (empty string).
-    ez::cli::Regular_parameter<"short-name", "",
+    ez::cli::Boolean_parameter<"short-name", "long-name",
         "",
-        [](auto arg) {
-            return arg;
-        },
-        [] {
-            return "default value"sv;
-        }
+        [](std::true_type) { return 123; },
+        [](std::false_type) { return 321; }
     >,
 
-    // A parameter description is missing (empty string).
-    ez::cli::Regular_parameter<"", "long-name",
-        "",
-        [](auto arg) {
-            return arg;
-        },
-        [] {
-            return "default value"sv;
-        }
+    // An true-value and false-value functions are missing.
+    ez::cli::Boolean_parameter<"short-name", "",
+        "The parameter descritption."
     >,
 
-    // An argument value-parser funciton is missing.
-    ez::cli::Regular_parameter<"short-name", "",
-        "A descritption of the param.",
-        [] {
-            return "default value"sv;
-        }
+    // A true-value function is missing.
+    ez::cli::Boolean_parameter<"short-name", "long-name",
+        "The parameter descritption.",
+        /*[](std::true_type) { return 123; },*/
+        [](std::false_type) { return 321; }
+    >,
+
+    // A false-value function is missing.
+    ez::cli::Boolean_parameter<"short-name", "long-name",
+        "The parameter descritption.",
+        [](std::true_type) { return 123; }/*,
+        [](std::false_type) { return 321; }*/
     >
 >;
+
+} // namespace
 
 TEMPLATE_LIST_TEST_CASE("I", "", Incorrect_parameter_types)
 {
---
+++ tests/custom_boolean_parameter.test.cpp
@@ -0,0 +1,258 @@
+#include <catch2/catch_all.hpp>
+#include "ez/cli/parameter/concepts.h"
+
+using ez::cli::api::Positional_parameter;
+using ez::cli::api::Regular_parameter;
+using ez::cli::api::Boolean_parameter;
+using ez::cli::api::Parameter;
+using namespace std::string_view_literals;
+
+
+struct Boolean_param_1 {
+    static constexpr auto short_name = "short-name"sv;
+    static constexpr auto description = "The parameter descritption."sv;
+
+    static constexpr auto true_value()
+    {
+        return 123;
+    }
+
+    static constexpr auto false_value()
+    {
+        return 321;
+    }
+};
+
+TEST_CASE("Boolean parameter may have only short name")
+{
+    using P = Boolean_param_1;
+
+    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
+    STATIC_REQUIRE_FALSE(Regular_parameter<P>);
+    STATIC_REQUIRE(Boolean_parameter<P>);
+    STATIC_REQUIRE(Parameter<P>);
+
+    namespace details_ = ez::cli::api::details_;
+    STATIC_REQUIRE(details_::Has_short_name<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_long_name<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
+    STATIC_REQUIRE(details_::Has_description<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_value_parser<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_default_value<P>);
+    STATIC_REQUIRE(details_::Has_true_value<P>);
+    STATIC_REQUIRE(details_::Has_false_value<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
+}
+
+struct Boolean_param_2 {
+    static constexpr auto long_name = "long-name"sv;
+    static constexpr auto description = "The parameter descritption."sv;
+
+    static constexpr auto true_value()
+    {
+        return 123;
+    }
+
+    static constexpr auto false_value()
+    {
+        return 321;
+    }
+};
+
+TEST_CASE("Boolean parameter may have only long name")
+{
+    using P = Boolean_param_2;
+
+    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
+    STATIC_REQUIRE_FALSE(Regular_parameter<P>);
+    STATIC_REQUIRE(Boolean_parameter<P>);
+    STATIC_REQUIRE(Parameter<P>);
+
+    namespace details_ = ez::cli::api::details_;
+    STATIC_REQUIRE_FALSE(details_::Has_short_name<P>);
+    STATIC_REQUIRE(details_::Has_long_name<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
+    STATIC_REQUIRE(details_::Has_description<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_value_parser<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_default_value<P>);
+    STATIC_REQUIRE(details_::Has_true_value<P>);
+    STATIC_REQUIRE(details_::Has_false_value<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
+}
+
+struct Boolean_param_3 {
+    static constexpr auto short_name = "short-name"sv;
+    static constexpr auto long_name = "long-name"sv;
+    static constexpr auto description = "The parameter descritption."sv;
+
+    static constexpr auto true_value()
+    {
+        return 123;
+    }
+
+    static constexpr auto false_value()
+    {
+        return 321;
+    }
+};
+
+TEST_CASE("Boolean parameter may have both short and long names")
+{
+    using P = Boolean_param_3;
+
+    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
+    STATIC_REQUIRE_FALSE(Regular_parameter<P>);
+    STATIC_REQUIRE(Boolean_parameter<P>);
+    STATIC_REQUIRE(Parameter<P>);
+
+    namespace details_ = ez::cli::api::details_;
+    STATIC_REQUIRE(details_::Has_short_name<P>);
+    STATIC_REQUIRE(details_::Has_long_name<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_unit_name<P>);
+    STATIC_REQUIRE(details_::Has_description<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_value_parser<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_default_value<P>);
+    STATIC_REQUIRE(details_::Has_true_value<P>);
+    STATIC_REQUIRE(details_::Has_false_value<P>);
+    STATIC_REQUIRE_FALSE(details_::Has_parse_repeated_value<P>);
+}
+
+
+namespace {
+
+
+template<ez::utils::Static_string str>
+struct Mock_short_name {
+    static constexpr auto short_name = std::string_view{str.value};
+};
+
+template<ez::utils::Static_string str>
+struct Mock_long_name {
+    static constexpr auto long_name = std::string_view{str.value};
+};
+
+template<ez::utils::Static_string str>
+struct Mock_description {
+    static constexpr auto description = std::string_view{str.value};
+};
+
+
+struct Mock_true_value_func {
+    static constexpr auto true_value()
+    {
+        return 123;
+    }
+};
+
+
+struct Mock_false_value_func {
+    static constexpr auto false_value()
+    {
+        return 321;
+    }
+};
+
+
+template<typename... Properties>
+struct Compose_test_param : Properties... {};
+
+
+using Incorrect_parameter_types = std::tuple<
+    // A parameter name is not specified.
+    Compose_test_param<
+        /* Mock_short_name<"">, */
+        /* Mock_long_name<"">, */
+        Mock_description<"The parameter descritption.">,
+        Mock_true_value_func,
+        Mock_false_value_func
+    >,
+
+    // A parameter name is not specified (an empty string).
+    Compose_test_param<
+        Mock_short_name<"">,
+        /* Mock_long_name<"">, */
+        Mock_description<"The parameter descritption.">,
+        Mock_true_value_func,
+        Mock_false_value_func
+    >,
+
+    // A parameter name is not specified (empty string).
+    Compose_test_param<
+        /* Mock_short_name<"">, */
+        Mock_long_name<"">,
+        Mock_description<"The parameter descritption.">,
+        Mock_true_value_func,
+        Mock_false_value_func
+    >,
+
+    // A parameter name is not specified (empty string).
+    Compose_test_param<
+        Mock_short_name<"">,
+        Mock_long_name<"">,
+        Mock_description<"The parameter descritption.">,
+        Mock_true_value_func,
+        Mock_false_value_func
+    >,
+
+    // A parameter description is missing.
+    Compose_test_param<
+        Mock_short_name<"short-name">,
+        Mock_long_name<"long-name">,
+        /* Mock_description<"The parameter descritption.">, */
+        Mock_true_value_func,
+        Mock_false_value_func
+    >,
+
+    // A parameter description is missing (empty string).
+    Compose_test_param<
+        Mock_short_name<"short-name">,
+        Mock_long_name<"long-name">,
+        Mock_description<"">,
+        Mock_true_value_func,
+        Mock_false_value_func
+    >,
+
+    // A parameter true-value and false-value functions are missing.
+    Compose_test_param<
+        Mock_short_name<"">,
+        Mock_long_name<"">,
+        Mock_description<"The parameter descritption.">
+        /* Mock_true_value_func, */
+        /* Mock_false_value_func */
+    >,
+
+    // A parameter true-value function is missing.
+    Compose_test_param<
+        Mock_short_name<"">,
+        Mock_long_name<"">,
+        Mock_description<"The parameter descritption.">,
+        /* Mock_true_value_func, */
+        Mock_false_value_func
+    >,
+
+    // A parameter false-value function is missing.
+    Compose_test_param<
+        Mock_short_name<"">,
+        Mock_long_name<"">,
+        Mock_description<"The parameter descritption.">,
+        Mock_true_value_func
+        /* Mock_false_value_func */
+    >
+>;
+
+} // namespace
+
+TEMPLATE_LIST_TEST_CASE("I", "", Incorrect_parameter_types)
+{
+    using P = TestType;
+
+    static_assert(!std::is_same_v<P, Incorrect_parameter_types>,
+        "Ensure using TEMPLATE_LIST_TEST_CASE and not TEMPLATE_TEST_CASE.");
+
+    STATIC_REQUIRE_FALSE(Positional_parameter<P>);
+    STATIC_REQUIRE_FALSE(Regular_parameter<P>);
+    STATIC_REQUIRE_FALSE(Boolean_parameter<P>);
+    STATIC_REQUIRE_FALSE(Parameter<P>);
+}
+
+
